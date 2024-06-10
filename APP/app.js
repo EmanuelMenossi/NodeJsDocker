a9c73bd262fef2631599f19c4f3e8daecd5953f9
Aqu1les <manuelmenossi@gmail.com>
2024-06-10 00:18:49 -0300

    Add docker

--- APP/app.js
+++ APP/app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const os = require('os')
 const app = express()
 
+app.get('/', (req, res) => {
+    return res
+        .status(200)
+        .json({
+            message: "ON",
+        });
+});
 
 app.get('/liveness', (req, res) => {
     return res
@@ -26,6 +33,18 @@ app.get('/readiness', (req, res) => {
             date: new Date().getTime()
          })
     })
+
+    app.get('/consulta-dados', (req, res) => {
+        return res
+            .status(200)
+            .json({
+                message: "Meu servidor está pronto para receber requisições!",
+                platform: os.platform(),
+                freemem: os.freemem(),
+                homedir: os.homedir(),
+                date: new Date().getTime()
+             })
+        })
     
     
     
