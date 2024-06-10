faf593c13e812568bc45a9807fe94e5c6b57edf2
Aqu1les <manuelmenossi@gmail.com>
2024-06-09 23:14:14 -0300

    App create

---
+++ APP/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const os = require('os')
+const app = express()
+
+
+app.get('/liveness', (req, res) => {
+    return res
+        .status(200)
+        .json({
+            message: "Meu servidor está vivo!",
+            path: process.cwd(),
+            gid: process.getgid ? process.getgid() : null,
+            uid: process.getuid ? process.getuid() : null,
+            date: new Date().getTime()
+        });
+});
+
+app.get('/readiness', (req, res) => {
+    return res
+        .status(200)
+        .json({
+            message: "Meu servidor está pronto para receber requisições!",
+            platform: os.platform(),
+            freemem: os.freemem(),
+            homedir: os.homedir(),
+            date: new Date().getTime()
+         })
+    })
+    
+    
+    
+
+
+module.exports = app
